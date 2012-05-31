67ee95e803a0f3873885886217612655d6a1fb7d
DrisyaNair <drisya.nair@techvantagesystems.com>
2012-05-31 00:54:56 -0700

    fdg

--- IHealth/Default.aspx.cs
+++ IHealth/Default.aspx.cs
@@ -11,8 +11,7 @@ namespace IHealth
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-          // jnininiuo
-            //kokj
+        
         }
 
         private void change()
